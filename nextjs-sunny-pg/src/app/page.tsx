471b262cb1e1848616976d40038a571dfd79b14f
ahmadshanawar <ahmad.shanawar@hotmail.com>
2024-12-29 04:26:19 +0530

    final_fixes

--- nextjs-sunny-pg/src/app/page.tsx
+++ nextjs-sunny-pg/src/app/page.tsx
@@ -3,12 +3,17 @@ import { LandingPage } from "./components/LandingPage";
 import { useEffect } from "react";
 import { restoreSession } from "@/lib/sessions";
 import { useRouter } from "next/navigation";
+import useUserStore from "@/lib/store/userStore";
 
 export default function Home() {
   const router = useRouter();
+  const { setUserId, setRole, setStatus } = useUserStore();
   useEffect(() => {
     const result = restoreSession();
     if (!result) {
+      setRole(null);
+      setStatus(null);
+      setUserId(null);
       router.push("/login");
     }
   }, []);
