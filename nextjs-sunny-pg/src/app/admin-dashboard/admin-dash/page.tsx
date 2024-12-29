27d49fa3978e068bcb455ede104d168e888dd74f
ahmadshanawar <ahmad.shanawar@hotmail.com>
2024-12-30 03:25:13 +0530

    margin_top

--- nextjs-sunny-pg/src/app/admin-dashboard/admin-dash/page.tsx
+++ nextjs-sunny-pg/src/app/admin-dashboard/admin-dash/page.tsx
@@ -14,7 +14,7 @@ export default function AdminDash() {
   );
 
   return (
-    <div className="container mx-auto p-4 mt-4">
+    <div className="container mx-auto p-4 mt-8">
       <div className="flex justify-between items-center mb-4">
         <div className="flex space-x-4">
           <div>
