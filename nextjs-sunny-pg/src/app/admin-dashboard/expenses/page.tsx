4e1236af50aedc34596c39cd63e576bd83a86ed1
ahmadshanawar <ahmad.shanawar@hotmail.com>
2024-12-31 03:57:49 +0530

    expese_modal_height

--- nextjs-sunny-pg/src/app/admin-dashboard/expenses/page.tsx
+++ nextjs-sunny-pg/src/app/admin-dashboard/expenses/page.tsx
@@ -218,10 +218,10 @@ const ExpensesPage = () => {
       {isDialogOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="fixed inset-0 bg-black opacity-50"></div>
-          <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full h-[calc(100vh-200px)]">
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4  overflow-y-auto">
               <div className="sm:flex sm:items-start">
-                <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
+                <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left w-full">
                   <h3 className="text-lg leading-6 font-medium text-gray-900">
                     Add Expense
                   </h3>
@@ -287,7 +287,7 @@ const ExpensesPage = () => {
                 </div>
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <div className="bg-gray-50 px-4 py-3 sm:px-6">
               <button
                 type="button"
                 className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-800 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:ml-3 sm:w-auto sm:text-sm"
